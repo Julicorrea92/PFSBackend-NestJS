dd54c11fbf805acbdfb7d57f18b4e55ea4079a24
Julicorrea92 <julianleo92@hotmail.com>
2022-10-25 00:24:50 -0300

    Creación de clases

---
+++ objetos/Integrante.ts
@@ -0,0 +1,13 @@
+abstract class Integrante{
+
+    protected credencial: string;
+    protected apellidoNombres: string;
+    protected fechaNacimiento: string;
+    protected paisNacimiento: string;
+    protected deporte: string;
+    protected rol: string;
+    
+    public constructor(){}
+
+    
+}
